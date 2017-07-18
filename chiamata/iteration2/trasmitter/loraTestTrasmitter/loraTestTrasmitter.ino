c21f304e719a7c9bf75c797e52cab88c393cac75
Lorenzo Romagnoli <lorenzormgnl@gmail.com>
2017-07-18 22:56:11 +0200

    added some ble stuff - need to check if value are being sent from board to app

--- chiamata/iteration2/trasmitter/loraTestTrasmitter/loraTestTrasmitter.ino
+++ chiamata/iteration2/trasmitter/loraTestTrasmitter/loraTestTrasmitter.ino
@@ -1,10 +1,15 @@
+#include "Adafruit_BLE.h"
+#include "Adafruit_BluefruitLE_SPI.h"
+#include "Adafruit_BluefruitLE_UART.h"
+
+#include "BluefruitConfig.h"
 
 ////////////////////////////////////////////
 /////////////////CONNECTIONS////////////////
 ////////////////////////////////////////////
+
 int buttonPin = 13;
 int ledPin = 12;
-
 
 ////////////////////////////////////////////
 /////////////////STATE_MACHINE//////////////
@@ -16,25 +21,46 @@ int state = 0;
 ////////////////LORA_DEVICE_ID//////////////
 ////////////////////////////////////////////
 
-const word ID = 61309;   
-const word ID_RX = 61308;   
+const word ID = 61309;
+const word ID_RX = 61308;
 
-//const word ID = 61311;   
-//const word ID_RX = 61310;   
+//const word ID = 61311;
+//const word ID_RX = 61310;
 
-//const word ID = 61313;   
-//const word ID_RX = 613012;   
+//const word ID = 61313;
+//const word ID_RX = 613012;
 
+////////////////////////////////////////////
+/////////////////BLE_&_BEACON///////////////
+////////////////////////////////////////////
+#define BEACON_MAJOR               "0x0000"
+#define BEACON_MINOR               "0x0001"
+#define LOCAL_NAME                 "vineriaMinerva"
+
+int32_t charid_string;
+int32_t charid_number;
+
+Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
+boolean BLE_connected = false;
 
 void setup() {
   pinMode(buttonPin, INPUT_PULLUP);
   pinMode(ledPin, OUTPUT);
   initradio();
+
+  initBluetooth();
+  setupBeacon();
+  setupGatt();
+
 }
 
 void loop() {
   stateMachine();
+  ble.update(200);
+
 }
+
+
 
 void stateMachine() {
   switch (state) {
@@ -42,8 +68,7 @@ void stateMachine() {
 
       if (checkbutton()) {
         state = 1;
-        Serial.println("going to state 1");
-
+        Serial.println("Going to state 1");
         //effettuo la chiamata
         sendData();
         //accendo il led
@@ -54,8 +79,12 @@ void stateMachine() {
       //aspetto conferma ricezione
       if (receiveData()) {
         state = 2;
-        Serial.println("going to state2");
+        Serial.println("Going to state 2");
+        writeCharacteristic(1, 2);
+        readCharacteristic(1);
       }
+        getGATTList();
+
       //se rivevo conferma -> state=2
       break;
     case 2:
@@ -63,10 +92,26 @@ void stateMachine() {
       //aspetto e torno a stato 0
       //delay(2000);
       digitalWrite(ledPin, LOW);
-       Serial.println("resetting to state0");
+      Serial.println("Going to state 0");
 
       state = 0;
       break;
   }
 }
+
+void BLE_connected_callback(void) {
+  Serial.println( "Connected to BLE");
+  Serial.println( "Going to state 1...");
+  //accendo il led
+  digitalWrite(ledPin, HIGH);
+  sendData();
+  state = 1;
+  BLE_connected = true;
+}
+
+void BLE_disconnected_callback(void) {
+  Serial.println( F("Disconnected") );
+  BLE_connected = false;
+}
+
 
